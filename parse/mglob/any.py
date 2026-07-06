c3ea394c1abe51898ccf878b7fd40fde5c489320
mubarak-mustopha <mustophamubarack17@gmail.com>
2026-07-06 17:26:21 +0100

    mofify Any to use greedy matching

--- parse/mglob/any.py
+++ parse/mglob/any.py
@@ -4,8 +4,16 @@ class Any(Match):
     def __init__(self, rest=None):
         super().__init__(rest)
 
-    def _match(self, text, start=0):    
-        for i in range(start, len(text) + 1):
+    # def _match(self, text, start=0):    
+    #     for i in range(start, len(text) + 1):
+    #         end = self.rest._match(text, i)
+    #         if end == len(text):
+    #             return end
+        
+    #     return None
+
+    def _match(self, text, start):
+        for i in range(len(text), start - 1, -1):
             end = self.rest._match(text, i)
             if end == len(text):
                 return end
